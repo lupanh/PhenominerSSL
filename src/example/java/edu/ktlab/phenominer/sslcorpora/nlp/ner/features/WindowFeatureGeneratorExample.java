b6b526c2b414ef8e4723f02f8ac32fc73e023d1d
lupanh <lupanh@yahoo.com>
2014-02-28 01:06:29 +0700

    new update

--- src/example/java/edu/ktlab/phenominer/sslcorpora/nlp/ner/features/WindowFeatureGeneratorExample.java
+++ src/example/java/edu/ktlab/phenominer/sslcorpora/nlp/ner/features/WindowFeatureGeneratorExample.java
@@ -4,16 +4,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 import opennlp.tools.util.featuregen.AdaptiveFeatureGenerator;
+import opennlp.tools.util.featuregen.TokenFeatureGenerator;
 import opennlp.tools.util.featuregen.WindowFeatureGenerator;
 
 public class WindowFeatureGeneratorExample {
 	public static void main(String[] args) {
-		AdaptiveFeatureGenerator windowFeatureGenerator = new WindowFeatureGenerator(new IdentityFeatureGenerator(), 0, 0);
+		AdaptiveFeatureGenerator windowFeatureGenerator = new WindowFeatureGenerator(new TokenFeatureGenerator(), 1, 1);
 		List<String> features = new ArrayList<String>();
 		String[] testSentence = new String[] { "a", "b", "c", "d", "e", "f", "g", "h" };
 		int testTokenIndex = 2;
 
 		windowFeatureGenerator.createFeatures(features, testSentence, testTokenIndex, null);
+		for (String feature : features)
+			System.out.println(feature);
 	}
 
 }
